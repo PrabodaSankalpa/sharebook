3f7efc0bb8de7287612308e82f1abea613fb4b1a
PrabodaSankalpa <dgprabodasankalpa@gmail.com>
2023-08-05 10:36:14 +0530

    Add New Listings Form is Done

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Appbar from "./components/Appbar";
 import { useAuth } from "./context/AuthContext";
-import { Navigate, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import {
   Box,
   Button,
--- src/components/ContentArea.jsx
+++ src/components/ContentArea.jsx
@@ -1,12 +1,11 @@
 import { Box } from "@mui/material";
 import React from "react";
-import MyListings from "./MyListings";
-
+import AddNewListings from "./AddNewListings";
 
 export default function ContentArea() {
   return (
     <Box sx={{padding: 2}}>
-      <MyListings/>
+      <AddNewListings />
     </Box>
   );
 }
